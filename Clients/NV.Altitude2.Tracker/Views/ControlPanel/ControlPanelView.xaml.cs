e5fb1e18efb1db3be99a3019c27fef2210823a4c
nvsnkv <nvsnkv@live.com>
2017-04-21 22:03:50 +0300

    Control panel view, Pipeline controls

---
+++ Clients/NV.Altitude2.Tracker/Views/ControlPanel/ControlPanelView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace NV.Altitude2.Tracker.Views.ControlPanel
+{
+    public partial class ControlPanelView : PivotItem
+    {
+        public ControlPanelView()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
---
+++ Clients/NV.Altitude2.Tracker/Views/ControlPanel/PipelineControlView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace NV.Altitude2.Tracker.Views.ControlPanel
+{
+    public partial class PipelineControlView : Grid
+    {
+        public PipelineControlView()
+        {
+            InitializeComponent();
+        }
+    }
+}

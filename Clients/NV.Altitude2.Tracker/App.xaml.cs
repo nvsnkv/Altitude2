7e3d3f2a0496dd4a98474de05fc5bacd37d83d49
nvsnkv <nvsnkv@live.com>
2017-04-21 21:21:32 +0300

    empty tracker app

---
+++ Clients/NV.Altitude2.Tracker/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace NV.Altitude2.Tracker
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            MainPage page = Window.Current.Content as MainPage;
+
+            if (page == null)
+            {
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                page = new MainPage();
+                Window.Current.Content = page;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                Window.Current.Activate();
+            }
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}

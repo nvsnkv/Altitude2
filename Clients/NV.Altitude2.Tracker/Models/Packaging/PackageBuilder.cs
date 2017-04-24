e9a17ee8d4a55f1054419df5195d916829f5cf44
[NV] Nikita Senkov <nvsnkv@live.com>
2017-04-24 22:07:46 +0300

    Tracker & PackageBuilder statuses

--- Clients/NV.Altitude2.Tracker/Models/Packaging/PackageBuilder.cs
+++ Clients/NV.Altitude2.Tracker/Models/Packaging/PackageBuilder.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Threading.Tasks;
 using NV.Altitude2.Domain;
 using NV.Altitude2.Tracker.Models.Pipeline;
 
 namespace NV.Altitude2.Tracker.Models.Packaging
 {
-    internal sealed class PackageBuilder : InOutPipelineService<Measurement, List<Measurement>, GenericState>
+    internal sealed class PackageBuilder : InOutPipelineService<Measurement, List<Measurement>, GenericState>, INotifyCollectionChanged
     {
         private readonly IList<Measurement> _measurements = new List<Measurement>();
         private readonly object _listLock = new object();
@@ -31,6 +32,10 @@ namespace NV.Altitude2.Tracker.Models.Packaging
             get => _packageSize;
             set => _packageSize = value > 0 ? value : throw new ArgumentException("PackageSize should be positive!", nameof(value));
         }
+
+        public int MeasurementsCount => _measurements.Count;
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
 
         protected override Task<bool> DoInitialize()
         {
@@ -58,10 +63,12 @@ namespace NV.Altitude2.Tracker.Models.Packaging
             lock (_listLock)
             {
                 _measurements.Add(data);
+                RaiseCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, new[]{ data }));
                 if (_measurements.Count >= _packageSize)
                 {
                     package = new List<Measurement>(_measurements);
                     _measurements.Clear();
+                    RaiseCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
                 }
             }
 
@@ -69,6 +76,11 @@ namespace NV.Altitude2.Tracker.Models.Packaging
             {
                 await SendNext(package);
             }
+        }
+
+        private void RaiseCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            CollectionChanged?.Invoke(this, e);
         }
     }
 }
--- Clients/NV.Altitude2.Tracker/ViewModels/RightNow/RightNowViewModel.cs
+++ Clients/NV.Altitude2.Tracker/ViewModels/RightNow/RightNowViewModel.cs
@@ -1,15 +1,68 @@
-﻿using Windows.UI.Core;
+﻿using System;
+using Windows.UI.Core;
 using NV.Altitude2.Tracker.Models;
+using NV.Altitude2.Tracker.Models.Packaging;
 
 namespace NV.Altitude2.Tracker.ViewModels.RightNow
 {
     internal class RightNowViewModel : ViewModelBase
     {
+        private string _trackerState;
+        private int _measurementsCount;
+
         public RightNowViewModel(ServicesCollection collection, CoreDispatcher dispatcher) : base(dispatcher)
         {
             Position = new PositionViewModel(collection.LocationService, dispatcher);
+
+            collection.LocationService.StateChanged += async (o, e) => await Dispatch(() =>
+            {
+                switch (e.State)
+                {
+                    case Models.Location.LocationServiceState.Disabled:
+                        TrackerState = "Disabled";
+                        break;
+                    case Models.Location.LocationServiceState.Initializing:
+                        TrackerState = "Initializing";
+                        break;
+                    case Models.Location.LocationServiceState.NoData:
+                        TrackerState = "No data available";
+                        break;
+                    case Models.Location.LocationServiceState.Running:
+                        TrackerState = "Running!";
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            });
+
+            collection.PackageBuilder.CollectionChanged += async (o, e) => await Dispatch(() =>
+            {
+                MeasurementsCount = collection.PackageBuilder.MeasurementsCount;
+            });
         }
 
         public PositionViewModel Position { get; }
+
+        public string TrackerState  
+        {
+            get => _trackerState;
+            private set
+            {
+                if (value == _trackerState) return;
+                _trackerState = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int MeasurementsCount
+        {
+            get => _measurementsCount;
+            private set
+            {
+                if (value == _measurementsCount) return;
+                _measurementsCount = value;
+                RaisePropertyChanged();
+            }
+        }
     }
 }
